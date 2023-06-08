a595161a22c967771c4e45cbb951eca1a1796cd3
Jason Leung <40867693+Hareye@users.noreply.github.com>
2023-06-07 22:42:15 -0700

    Implemented animations and account registration
    - Added simple animation to login/register panel on Title Screen
    - Implemented account registration on Title Screen
    - Sends a request to server to create an account

--- Assets/Scripts/Managers/TitleManager.cs
+++ Assets/Scripts/Managers/TitleManager.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
 using TMPro;
@@ -8,42 +6,56 @@ using urls;
 public class TitleManager : MonoBehaviour
 {
     [SerializeField]
+    public GameObject panel;
     public GameObject login;
-
-    [SerializeField]
+    public GameObject register;
     public GameObject startButton;
 
-    private GameObject panelText;
-    private GameObject emailField;
-    private GameObject passwordField;
-    private GameObject loginButton;
-    private GameObject registerButton;
-    private GameObject forgotPassword;
-    private GameObject rememberMe;
+    private GameObject lEmailField;
+    private GameObject lPasswordField;
+    private GameObject lLoginButton;
+    private GameObject lRegisterButton;
+    private GameObject lForgotPassword;
+    private GameObject lRememberMe;
+
+    private GameObject rEmailField;
+    private GameObject rPasswordField;
+    private GameObject rRegisterButton;
+    private GameObject rEmailVerification;
+
+    private Animator panelAnimator;
 
     private ServerManager server;
     private Endpoints endpoints = new Endpoints();
 
     void Awake()
     {
-        panelText = login.transform.Find("Panel Text").gameObject;
-        emailField = login.transform.Find("Email").gameObject;
-        passwordField = login.transform.Find("Password").gameObject;
-        loginButton = login.transform.Find("Login Button").gameObject;
-        registerButton = login.transform.Find("Register Button").gameObject;
-        forgotPassword = login.transform.Find("Forgot Password").gameObject;
-        rememberMe = login.transform.Find("Remember Me").gameObject;
+        lEmailField = login.transform.Find("Email").gameObject;
+        lPasswordField = login.transform.Find("Password").gameObject;
+        lLoginButton = login.transform.Find("Login Button").gameObject;
+        lRegisterButton = login.transform.Find("Register Button").gameObject;
+        lForgotPassword = login.transform.Find("Forgot Password").gameObject;
+        lRememberMe = login.transform.Find("Remember Me").gameObject;
+
+        rEmailField = register.transform.Find("Email").gameObject;
+        rPasswordField = register.transform.Find("Password").gameObject;
+        rRegisterButton = register.transform.Find("Register Button").gameObject;
+        rEmailVerification = register.transform.Find("Email Verification").gameObject;
+
+        panelAnimator = panel.GetComponent<Animator>();
 
         server = GetComponent<ServerManager>();
 
-        emailField.GetComponent<TMP_InputField>().onValueChanged.AddListener(delegate { onEdit(); });
-        passwordField.GetComponent<TMP_InputField>().onValueChanged.AddListener(delegate { onEdit(); });
+        lEmailField.GetComponent<TMP_InputField>().onValueChanged.AddListener(delegate { onEdit(lEmailField, lPasswordField, lLoginButton); });
+        lPasswordField.GetComponent<TMP_InputField>().onValueChanged.AddListener(delegate { onEdit(lEmailField, lPasswordField, lLoginButton); });
+        rEmailField.GetComponent<TMP_InputField>().onValueChanged.AddListener(delegate { onEdit(rEmailField, rPasswordField, rRegisterButton); });
+        rPasswordField.GetComponent<TMP_InputField>().onValueChanged.AddListener(delegate { onEdit(rEmailField, rPasswordField, rRegisterButton); });
     }
 
     public void startLogin()
     {
-        string email = emailField.GetComponent<TMP_InputField>().text;
-        string password = passwordField.GetComponent<TMP_InputField>().text;
+        string email = lEmailField.GetComponent<TMP_InputField>().text;
+        string password = lPasswordField.GetComponent<TMP_InputField>().text;
 
         StartCoroutine(server.authenticateUser(endpoints.authenticate, email, password));
 
@@ -52,20 +64,29 @@ public class TitleManager : MonoBehaviour
 
     public void startRegister()
     {
+        string email = rEmailField.GetComponent<TMP_InputField>().text;
+        string password = rPasswordField.GetComponent<TMP_InputField>().text;
 
+        rEmailVerification.SetActive(true);
+        StartCoroutine(server.registerUser(endpoints.register, email, password));
     }
 
-    public void onEdit()
+    public void playPanelAnimation(string name)
+    {
+        panelAnimator.Play(name);
+    }
+
+    public void onEdit(GameObject emailField, GameObject passwordField, GameObject button)
     {
         string email = emailField.GetComponent<TMP_InputField>().text;
         string password = passwordField.GetComponent<TMP_InputField>().text;
 
         if (email.Length > 0 && password.Length > 0)
         {
-            loginButton.GetComponent<Image>().color = new Color32(148, 121, 173, 255);
+            button.GetComponent<Image>().color = new Color32(148, 121, 173, 255);
         } else
         {
-            loginButton.GetComponent<Image>().color = new Color32(106, 106, 106, 255);
+            button.GetComponent<Image>().color = new Color32(106, 106, 106, 255);
         }
     }
 
--- Assets/Scripts/ServerManager.cs
+++ Assets/Scripts/ServerManager.cs
@@ -2,9 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using urls;
 
 public class ServerManager : MonoBehaviour
 {
+    private Endpoints endpoints = new Endpoints();
+
     public IEnumerator authenticateUser(string uri, string email, string password)
     {
         List<IMultipartFormSection> formData = new List<IMultipartFormSection>();
@@ -21,6 +24,56 @@ public class ServerManager : MonoBehaviour
         else if (uwr.responseCode == 200)
         {
             Debug.Log("Received: " + uwr.downloadHandler.text);
+        }
+    }
+
+    public IEnumerator registerUser(string uri, string email, string password)
+    {
+        IEnumerator checkUserCoroutine = checkUser(endpoints.checkUser, email);
+        yield return StartCoroutine(checkUserCoroutine);
+
+        string userExists = checkUserCoroutine.Current as string;
+
+        if (userExists.Equals("false"))
+        {
+            List<IMultipartFormSection> formData = new List<IMultipartFormSection>();
+            formData.Add(new MultipartFormDataSection("email", email));
+            formData.Add(new MultipartFormDataSection("password", password));
+
+            UnityWebRequest uwr = UnityWebRequest.Post(uri, formData);
+            yield return uwr.SendWebRequest();
+
+            if (uwr.result == UnityWebRequest.Result.ConnectionError)
+            {
+                Debug.Log("Error while sending: " + uwr.error);
+            }
+            else if (uwr.responseCode == 200)
+            {
+                Debug.Log("Received: " + uwr.downloadHandler.text);
+            }
+        } else
+        {
+            // User already exists
+            yield break;
+        }
+    }
+
+    public IEnumerator checkUser(string uri, string email)
+    {
+        List<IMultipartFormSection> formData = new List<IMultipartFormSection>();
+        formData.Add(new MultipartFormDataSection("email", email));
+
+        UnityWebRequest uwr = UnityWebRequest.Post(uri, formData);
+        yield return uwr.SendWebRequest();
+
+        if (uwr.result == UnityWebRequest.Result.ConnectionError)
+        {
+            Debug.Log("Error while sending: " + uwr.error);
+        }
+        else if (uwr.responseCode == 200)
+        {
+            Debug.Log("Received: " + uwr.downloadHandler.text);
+            yield return uwr.downloadHandler.text;
         }
     }
 
