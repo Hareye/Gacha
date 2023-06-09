c4c4b56d51f7ff873b5e5162a48a78ea20097856
Jason Leung <40867693+Hareye@users.noreply.github.com>
2023-06-08 22:36:28 -0700

    Implemented remember me (auto login)

--- Assets/Scripts/Managers/TitleManager.cs
+++ Assets/Scripts/Managers/TitleManager.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Collections;
 using UnityEngine.UI;
 using UnityEngine;
 using TMPro;
@@ -14,7 +16,6 @@ public class TitleManager : MonoBehaviour
     private GameObject lEmailField;
     private GameObject lPasswordField;
     private GameObject lLoginButton;
-    private GameObject lRegisterButton;
     private GameObject lForgotPassword;
     private GameObject lRememberMe;
 
@@ -22,6 +23,8 @@ public class TitleManager : MonoBehaviour
     private GameObject rPasswordField;
     private GameObject rRegisterButton;
     private GameObject rEmailVerification;
+    private GameObject rUserExists;
+    private GameObject rInvalid;
 
     private Animator panelAnimator;
 
@@ -30,10 +33,15 @@ public class TitleManager : MonoBehaviour
 
     void Awake()
     {
+        if (!PlayerPrefs.HasKey("rememberMe"))
+        {
+            PlayerPrefs.SetInt("rememberMe", 0);
+            PlayerPrefs.Save();
+        }
+
         lEmailField = login.transform.Find("Email").gameObject;
         lPasswordField = login.transform.Find("Password").gameObject;
         lLoginButton = login.transform.Find("Login Button").gameObject;
-        lRegisterButton = login.transform.Find("Register Button").gameObject;
         lForgotPassword = login.transform.Find("Forgot Password").gameObject;
         lRememberMe = login.transform.Find("Remember Me").gameObject;
 
@@ -41,15 +49,37 @@ public class TitleManager : MonoBehaviour
         rPasswordField = register.transform.Find("Password").gameObject;
         rRegisterButton = register.transform.Find("Register Button").gameObject;
         rEmailVerification = register.transform.Find("Email Verification").gameObject;
+        rUserExists = register.transform.Find("User Exists").gameObject;
+        rInvalid = register.transform.Find("Invalid").gameObject;
 
         panelAnimator = panel.GetComponent<Animator>();
-
         server = GetComponent<ServerManager>();
 
         lEmailField.GetComponent<TMP_InputField>().onValueChanged.AddListener(delegate { onEdit(lEmailField, lPasswordField, lLoginButton); });
         lPasswordField.GetComponent<TMP_InputField>().onValueChanged.AddListener(delegate { onEdit(lEmailField, lPasswordField, lLoginButton); });
         rEmailField.GetComponent<TMP_InputField>().onValueChanged.AddListener(delegate { onEdit(rEmailField, rPasswordField, rRegisterButton); });
         rPasswordField.GetComponent<TMP_InputField>().onValueChanged.AddListener(delegate { onEdit(rEmailField, rPasswordField, rRegisterButton); });
+
+        if (PlayerPrefs.HasKey("email") && PlayerPrefs.GetInt("rememberMe") == 1)
+        {
+            lEmailField.GetComponent<TMP_InputField>().text = PlayerPrefs.GetString("email");
+            lRememberMe.GetComponent<Toggle>().isOn = true;
+
+            if (PlayerPrefs.HasKey("accessToken"))
+            {
+                Debug.Log("Auto Login");
+                StartCoroutine(loginProcess(PlayerPrefs.GetString("email"), "empty", PlayerPrefs.GetString("accessToken")));
+            }
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        if (PlayerPrefs.GetInt("rememberMe") == 0 && PlayerPrefs.HasKey("accessToken"))
+        {
+            PlayerPrefs.DeleteKey("accessToken");
+            PlayerPrefs.Save();
+        }
     }
 
     public void startLogin()
@@ -57,9 +87,22 @@ public class TitleManager : MonoBehaviour
         string email = lEmailField.GetComponent<TMP_InputField>().text;
         string password = lPasswordField.GetComponent<TMP_InputField>().text;
 
-        StartCoroutine(server.authenticateUser(endpoints.authenticate, email, password));
+        if (lRememberMe.GetComponent<Toggle>().isOn)
+        {
+            Debug.Log("Remember Me: On");
+            PlayerPrefs.SetInt("rememberMe", 1);
+            PlayerPrefs.SetString("email", email);
+            PlayerPrefs.Save();
+        }
+        else
+        {
+            Debug.Log("Remember Me: Off");
+            PlayerPrefs.SetInt("rememberMe", 0);
+            PlayerPrefs.DeleteKey("email");
+            PlayerPrefs.Save();
+        }
 
-        //startButton.SetActive(true);
+        StartCoroutine(loginProcess(email, password, "empty"));
     }
 
     public void startRegister()
@@ -67,8 +110,7 @@ public class TitleManager : MonoBehaviour
         string email = rEmailField.GetComponent<TMP_InputField>().text;
         string password = rPasswordField.GetComponent<TMP_InputField>().text;
 
-        rEmailVerification.SetActive(true);
-        StartCoroutine(server.registerUser(endpoints.register, email, password));
+        StartCoroutine(registerProcess(email, password));
     }
 
     public void playPanelAnimation(string name)
@@ -93,5 +135,62 @@ public class TitleManager : MonoBehaviour
     public void disableButton()
     {
         startButton.SetActive(false);
+    }
+
+    public IEnumerator registerProcess(string email, string password)
+    {
+        IEnumerator registerCoroutine = server.registerUser(endpoints.register, email, password);
+        yield return StartCoroutine(registerCoroutine);
+
+        string status = registerCoroutine.Current as string;
+
+        if (status.Equals("invalid"))
+        {
+            // Invalid email or password inputs
+            rEmailField.GetComponent<Outline>().enabled = true;
+            rPasswordField.GetComponent<Outline>().enabled = true;
+            rEmailVerification.SetActive(false);
+            rUserExists.SetActive(false);
+            rInvalid.SetActive(true);
+        } else if (status.Equals("true"))
+        {
+            // Register success
+            rEmailField.GetComponent<Outline>().enabled = false;
+            rPasswordField.GetComponent<Outline>().enabled = false;
+            rEmailVerification.SetActive(true);
+            rUserExists.SetActive(false);
+            rInvalid.SetActive(false);
+        } else
+        {
+            // Register failed (user already exists)
+            rEmailField.GetComponent<Outline>().enabled = true;
+            rPasswordField.GetComponent<Outline>().enabled = true;
+            rEmailVerification.SetActive(false);
+            rUserExists.SetActive(true);
+            rInvalid.SetActive(false);
+        }
+    }
+
+    public IEnumerator loginProcess(string email, string password, string accessToken)
+    {
+        IEnumerator loginCoroutine = server.loginUser(endpoints.login, email, password, accessToken);
+        yield return StartCoroutine(loginCoroutine);
+
+        string status = loginCoroutine.Current as string;
+
+        if (status.Equals("true"))
+        {
+            // Login success
+            lEmailField.GetComponent<Outline>().enabled = false;
+            lPasswordField.GetComponent<Outline>().enabled = false;
+        } else if (status.Equals("invalid"))
+        {
+            // Login failed (invalid inputs or user failed to verify)
+            lEmailField.GetComponent<Outline>().enabled = true;
+            lPasswordField.GetComponent<Outline>().enabled = true;
+        } else
+        {
+            // Access token expired
+        }
     }
 }
--- Assets/Scripts/ServerManager.cs
+++ Assets/Scripts/ServerManager.cs
@@ -2,17 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 using urls;
 
 public class ServerManager : MonoBehaviour
 {
     private Endpoints endpoints = new Endpoints();
 
-    public IEnumerator authenticateUser(string uri, string email, string password)
+    public IEnumerator loginUser(string uri, string email, string password, string accessToken)
     {
         List<IMultipartFormSection> formData = new List<IMultipartFormSection>();
         formData.Add(new MultipartFormDataSection("email", email));
         formData.Add(new MultipartFormDataSection("password", password));
+        formData.Add(new MultipartFormDataSection("accessToken", accessToken));
 
         UnityWebRequest uwr = UnityWebRequest.Post(uri, formData);
         yield return uwr.SendWebRequest();
@@ -23,7 +25,20 @@ public class ServerManager : MonoBehaviour
         }
         else if (uwr.responseCode == 200)
         {
-            Debug.Log("Received: " + uwr.downloadHandler.text);
+            Debug.Log("loginUser: " + uwr.downloadHandler.text);
+
+            if (!uwr.downloadHandler.text.Equals("failed") && !uwr.downloadHandler.text.Equals("expired"))
+            {
+                PlayerPrefs.SetString("accessToken", uwr.downloadHandler.text);
+                PlayerPrefs.Save();
+                yield return "true";
+            } else if (uwr.downloadHandler.text.Equals("failed"))
+            {
+                yield return "invalid";
+            } else
+            {
+                yield return "expired";
+            }
         }
     }
 
@@ -34,27 +49,40 @@ public class ServerManager : MonoBehaviour
 
         string userExists = checkUserCoroutine.Current as string;
 
-        if (userExists.Equals("false"))
+        // Check if inputs are valid
+        if (!userExists.Equals("failed"))
         {
-            List<IMultipartFormSection> formData = new List<IMultipartFormSection>();
-            formData.Add(new MultipartFormDataSection("email", email));
-            formData.Add(new MultipartFormDataSection("password", password));
-
-            UnityWebRequest uwr = UnityWebRequest.Post(uri, formData);
-            yield return uwr.SendWebRequest();
-
-            if (uwr.result == UnityWebRequest.Result.ConnectionError)
+            // Check if user exists
+            if (userExists.Equals("false"))
             {
-                Debug.Log("Error while sending: " + uwr.error);
-            }
-            else if (uwr.responseCode == 200)
+                List<IMultipartFormSection> formData = new List<IMultipartFormSection>();
+                formData.Add(new MultipartFormDataSection("email", email));
+                formData.Add(new MultipartFormDataSection("password", password));
+
+                UnityWebRequest uwr = UnityWebRequest.Post(uri, formData);
+                yield return uwr.SendWebRequest();
+
+                if (uwr.result == UnityWebRequest.Result.ConnectionError)
+                {
+                    Debug.Log("Error while sending: " + uwr.error);
+                } else
+                {
+                    Debug.Log("registerUser: " + uwr.downloadHandler.text);
+                    if (uwr.downloadHandler.text.Equals("failed"))
+                    {
+                        yield return "invalid";
+                    } else
+                    {
+                        yield return "true";
+                    }
+                }
+            } else
             {
-                Debug.Log("Received: " + uwr.downloadHandler.text);
+                yield return "false";
             }
         } else
         {
-            // User already exists
-            yield break;
+            yield return "invalid";
         }
     }
 
@@ -72,23 +100,8 @@ public class ServerManager : MonoBehaviour
         }
         else if (uwr.responseCode == 200)
         {
-            Debug.Log("Received: " + uwr.downloadHandler.text);
+            Debug.Log("checkUser: " + uwr.downloadHandler.text);
             yield return uwr.downloadHandler.text;
-        }
-    }
-
-    public IEnumerator getRequest(string uri)
-    {
-        UnityWebRequest uwr = UnityWebRequest.Get(uri);
-        yield return uwr.SendWebRequest();
-
-        if (uwr.result == UnityWebRequest.Result.ConnectionError)
-        {
-            Debug.Log("Error while sending: " + uwr.error);
-        }
-        else if (uwr.responseCode == 200)
-        {
-            Debug.Log("Received: " + uwr.downloadHandler.text);
         }
     }
 }
